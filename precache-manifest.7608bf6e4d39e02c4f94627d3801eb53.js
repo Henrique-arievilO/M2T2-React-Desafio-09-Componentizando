ad5fcdfaa6285c9226f143ea23fca78f8ef6e466
Wanderson Henriqe Oliveira <wandholiveira1@gmail.com>
2022-08-11 16:37:12 -0300

    Updates

---
+++ precache-manifest.7608bf6e4d39e02c4f94627d3801eb53.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff4f0eb45eeb35738dc0",
+    "url": "/M2T2-React-Desafio-09-Componentizando/static/js/main.ff4f0eb4.chunk.js"
+  },
+  {
+    "revision": "7b8fce782801e248cb75",
+    "url": "/M2T2-React-Desafio-09-Componentizando/static/js/1.7b8fce78.chunk.js"
+  },
+  {
+    "revision": "3c9bce5ed7e1c7f05d3c",
+    "url": "/M2T2-React-Desafio-09-Componentizando/static/js/runtime~main.3c9bce5e.js"
+  },
+  {
+    "revision": "91cecec3d5ae8d255526ccd1b4579cf5",
+    "url": "/M2T2-React-Desafio-09-Componentizando/static/media/wallpaper.91cecec3.jpg"
+  },
+  {
+    "revision": "92dc25f76f0182c1aa282dbe622b487e",
+    "url": "/M2T2-React-Desafio-09-Componentizando/static/media/p3.92dc25f7.jpeg"
+  },
+  {
+    "revision": "3738a3e74eccbf5909b1f92359292675",
+    "url": "/M2T2-React-Desafio-09-Componentizando/static/media/p2.3738a3e7.jpeg"
+  },
+  {
+    "revision": "a488eddcd23018f0e120b2c9cb63936a",
+    "url": "/M2T2-React-Desafio-09-Componentizando/static/media/p1.a488eddc.jpeg"
+  },
+  {
+    "revision": "c09c66d07a7df8d039005d340828f701",
+    "url": "/M2T2-React-Desafio-09-Componentizando/static/media/autor.c09c66d0.jpg"
+  },
+  {
+    "revision": "979ed1c61b8d9380fb26d09eb0cc6bcb",
+    "url": "/M2T2-React-Desafio-09-Componentizando/index.html"
+  }
+];
